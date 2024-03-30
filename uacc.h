816414f39c92fcdb4f8ad8e9b0aa3764db2f6c2b
Artem Pirunov <psycoderx@gmail.com>
2024-03-30 16:08:10 +0200

    Implement string view

--- uacc.h
+++ uacc.h
@@ -33,6 +33,11 @@ extern "C" {
 /*----------------------------------------------------------*/
 
 /*
+Unsigned 64-bit integer.
+*/
+typedef unsigned long uint64;
+
+/*
 String buffer.
 */
 typedef struct Strbuf {
@@ -41,6 +46,14 @@ typedef struct Strbuf {
   int capacity;
   int is_inited;
 } Strbuf;
+
+/*
+String view.
+*/
+typedef struct Strview {
+  const char *at;
+  int length;
+} Strview;
 
 /*
 Global variables.
@@ -122,6 +135,7 @@ sb_insert   | Insert a string
 sb_remove   | Remove characters
 sb_replace  | Replace characters with a string
 sb_reserve  | Reserve memory for characters
+sb_view     | Make a string view
 */
 
 /*
@@ -195,6 +209,209 @@ Prepare `sb` to store at least `cap` characters.
 */
 void
 sb_reserve(Strbuf *sb, int cap);
+
+/*
+String view from `sb`.
+*/
+Strview
+sb_view(Strbuf *sb);
+
+/*----------------------------------------------------------*/
+/* FUNCTIONS: STRING VIEW                                   */
+/*----------------------------------------------------------*/
+
+/*
+    GLOSSARY
+
+sv_array          | View an array
+sv_compare        | Compare two strings
+sv_contains_char  | Check if the string contains the character
+sv_contains_sv    | Check if the string contains another
+sv_cstr           | View a zero terminated string
+sv_cut            | Remove the first characters
+sv_cut_end        | Remove the last characters
+sv_equal          | Check if two strings are equal
+sv_equal_no_case  | Check if two strings are equal
+sv_filter         | Count from the start while filter
+sv_filter_end     | Count from the end while filter
+sv_filter_not     | Count from the start until filter
+sv_filter_not_end | Count from the end until filter
+sv_find_char      | Find the first such character
+sv_find_char_end  | Find the last such character
+sv_find_sv        | Find the first such string
+sv_find_sv_end    | Find the last such string
+sv_get            | Take the first characters
+sv_get_end        | Take the last characters
+sv_prefix         | Check if the string starts with prefix
+sv_span           | Count from the start while in the sample
+sv_span_end       | Count from the end while in the sample
+sv_span_not       | Count from the start until in the sample
+sv_span_not_end   | Count from the end until in the sample
+sv_substr         | Get the sub-string from the string
+sv_suffix         | Check if the string ends with suffix
+
+*/
+
+/*
+String view from an array of characters.
+*/
+Strview
+sv_array(const char *chars, int length);
+
+/*
+Compare `string` to `another`.
+*/
+int
+sv_compare(Strview string, Strview another);
+
+/*
+Check if `string` contains `ch`.
+*/
+int
+sv_contains_char(Strview string, char ch);
+
+/*
+Check if `string` contains `substr`.
+*/
+int
+sv_contains_sv(Strview string, Strview substr);
+
+/*
+String view from a zero terminated string.
+*/
+Strview
+sv_cstr(const char *cstr);
+
+/*
+Remove the first `count` characters from `string`.
+*/
+Strview
+sv_cut(Strview string, int count);
+
+/*
+Remove the last `count` characters from `string`.
+*/
+Strview
+sv_cut_end(Strview string, int count);
+
+/*
+Check if `string` equal `another`.
+*/
+int
+sv_equal(Strview string, Strview another);
+
+/*
+Check if `string` equal `another`. Ignore the differnce
+between upper case and lower case.
+*/
+int
+sv_equal_no_case(Strview string, Strview another);
+
+/*
+Count the first characters in `string` that pass `filter`.
+*/
+int
+sv_filter(Strview string, int(*filter)(int));
+
+/*
+Count the last characters in `string` that pass `filter`.
+*/
+int
+sv_filter_end(Strview string, int(*filter)(int));
+
+/*
+Count the first characters in `string` that did not
+pass `filter`.
+*/
+int
+sv_filter_not(Strview string, int(*filter)(int));
+
+/*
+Count the last characters in `string` that did not
+pass `filter`.
+*/
+int
+sv_filter_not_end(Strview string, int(*filter)(int));
+
+/*
+Find the first apearance of `ch` in `string`.
+*/
+int
+sv_find_char(Strview string, char ch);
+
+/*
+Find the last apearance of `ch` in `string`.
+*/
+int
+sv_find_char_end(Strview string, char ch);
+
+/*
+Find the first apearance of `substr` in `string`.
+*/
+int
+sv_find_sv(Strview string, Strview substr);
+
+/*
+Find the last apearance of `substr` in `string`.
+*/
+int
+sv_find_sv_end(Strview string, Strview substr);
+
+/*
+Get the first `count` characters from `string`.
+*/
+Strview
+sv_get(Strview string, int count);
+
+/*
+Get the last `count` characters from `string`.
+*/
+Strview
+sv_get_end(Strview string, int count);
+
+/*
+Check if `string` starts with `prefix`.
+*/
+int
+sv_prefix(Strview string, Strview prefix);
+
+/*
+Count the first characters from `string` that are in `sample`.
+*/
+int
+sv_span(Strview string, Strview sample);
+
+/*
+Count the last characters from `string` that are in `sample`.
+*/
+int
+sv_span_end(Strview string, Strview sample);
+
+/*
+Count the first characters from `string` that are not
+in `sample`.
+*/
+int
+sv_span_not(Strview string, Strview sample);
+
+/*
+Count the last characters from `string` that are not
+in `sample`.
+*/
+int
+sv_span_not_end(Strview string, Strview sample);
+
+/*
+Get `length` characters from `start` in `string`.
+*/
+Strview
+sv_substr(Strview string, int start, int length);
+
+/*
+Check if `string` ends with `suffix`.
+*/
+int
+sv_suffix(Strview string, Strview suffix);
 
 /*----------------------------------------------------------*/
 /* VARIABLES                                                */
--- uacc_lib.c
+++ uacc_lib.c
@@ -321,6 +321,370 @@ sb_vreplace(Strbuf *sb, int i, int n,
 }
 
 /*----------------------------------------------------------*/
+/* IMPLEMENTATION: STRING VIEW                              */
+/*----------------------------------------------------------*/
+
+/*----------------------------------------------------------*/
+Strview
+sv_array(const char *chars, int length)
+{
+  Strview sv;
+  /**/
+  assert(chars != NULL);
+  assert(length >= 0);
+  /**/
+  mem_clear(&sv, sizeof(sv));
+  sv.length = length;
+  sv.at = chars;
+  return sv;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_compare(Strview string, Strview another)
+{
+  if (string.length > another.length) {
+    return 1;
+  }
+  if (string.length < another.length) {
+    return -1;
+  }
+  return memcmp(string.at, another.at, string.length);
+}
+
+/*----------------------------------------------------------*/
+int
+sv_contains_char(Strview string, char ch)
+{
+  const char *ptr = NULL;
+  /**/
+  ptr = memchr(string.at, ch, string.length);
+  return ptr != NULL;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_contains_sv(Strview string, Strview substr)
+{
+  const char *part = NULL;
+  int i = 0;
+  int maxi = 0;
+  /**/
+  maxi = string.length - substr.length;
+  for (i = 0; i <= maxi; i++) {
+    part = string.at + i;
+    if (memcmp(part, substr.at, substr.length) == 0) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+/*----------------------------------------------------------*/
+Strview
+sv_cstr(const char *cstr)
+{
+  assert(cstr != NULL);
+  return sv_array(cstr, strlen(cstr));
+}
+
+/*----------------------------------------------------------*/
+Strview
+sv_cut(Strview string, int count)
+{
+  assert(count >= 0);
+  /**/
+  if (count > string.length) {
+    count = string.length;
+  }
+  return sv_array(string.at + count, string.length - count);
+}
+
+/*----------------------------------------------------------*/
+Strview
+sv_cut_end(Strview string, int count)
+{
+  assert(count >= 0);
+  /**/
+  if (count > string.length) {
+    count = string.length;
+  }
+  return sv_array(string.at, string.length - count);
+}
+
+/*----------------------------------------------------------*/
+int
+sv_equal(Strview string, Strview another)
+{
+  if (string.length != another.length) {
+    return 0;
+  }
+  return memcmp(string.at, another.at, string.length) == 0;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_equal_no_case(Strview string, Strview another)
+{
+  int i = 0;
+  /**/
+  if (string.length != another.length) {
+    return 0;
+  }
+  for (i = 0; i < string.length; i++) {
+    if (toupper(string.at[i]) != toupper(another.at[i])) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_filter(Strview string, int(*filter)(int))
+{
+  int i = 0;
+  /**/
+  for (i = 0; i < string.length; i++) {
+    if (!filter(string.at[i])) {
+      return i;
+    }
+  }
+  return string.length;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_filter_end(Strview string, int(*filter)(int))
+{
+  int i = 0;
+  int maxi = string.length - 1;
+  /**/
+  for (i = maxi; 0 <= i; i--) {
+    if (!filter(string.at[i])) {
+      return maxi - i;
+    }
+  }
+  return string.length;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_filter_not(Strview string, int(*filter)(int))
+{
+  int i = 0;
+  /**/
+  for (i = 0; i < string.length; i++) {
+    if (filter(string.at[i])) {
+      return i;
+    }
+  }
+  return string.length;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_filter_not_end(Strview string, int(*filter)(int))
+{
+  int i = 0;
+  int maxi = string.length - 1;
+  /**/
+  for (i = maxi; 0 <= i; i--) {
+    if (filter(string.at[i])) {
+      return maxi - i;
+    }
+  }
+  return string.length;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_find_char(Strview string, char ch)
+{
+  const char *ptr = NULL;
+  /**/
+  ptr = memchr(string.at, ch, string.length);
+  if (ptr == NULL) {
+    return -1;
+  }
+  return ptr - string.at;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_find_char_end(Strview string, char ch)
+{
+  int i = 0;
+  /**/
+  for (i = string.length - 1; 0 <= i; i--) {
+    if (string.at[i] == ch) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_find_sv(Strview string, Strview substr)
+{
+  const char *part = NULL;
+  int i = 0;
+  int maxi = 0;
+  /**/
+  maxi = string.length - substr.length;
+  for (i = 0; i <= maxi; i++) {
+    part = string.at + i;
+    if (memcmp(part, substr.at, substr.length) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_find_sv_end(Strview string, Strview substr)
+{
+  const char *part = NULL;
+  int i = 0;
+  int maxi = 0;
+  /**/
+  maxi = string.length - substr.length;
+  for (i = maxi; 0 <= i; i--) {
+    part = string.at + i;
+    if (memcmp(part, substr.at, substr.length) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+/*----------------------------------------------------------*/
+Strview
+sv_get(Strview string, int count)
+{
+  assert(count >= 0);
+  /**/
+  if (count > string.length) {
+    count = string.length;
+  }
+  return sv_array(string.at, count);
+}
+
+/*----------------------------------------------------------*/
+Strview
+sv_get_end(Strview string, int count)
+{
+  assert(count >= 0);
+  /**/
+  if (count > string.length) {
+    count = string.length;
+  }
+  return sv_array(string.at + string.length - count, count);
+}
+
+/*----------------------------------------------------------*/
+int
+sv_prefix(Strview string, Strview prefix)
+{
+  if (string.length < prefix.length) {
+    return 0;
+  }
+  return memcmp(string.at, prefix.at, prefix.length) == 0;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_span(Strview string, Strview sample)
+{
+  const char *cp = NULL;
+  int i = 0;
+  /**/
+  for (i = 0; i < string.length; i++) {
+    cp = memchr(sample.at, string.at[i], sample.length);
+    if (cp != NULL) {
+      return i;
+    }
+  }
+  return string.length;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_span_end(Strview string, Strview sample)
+{
+  const char *cp = NULL;
+  int i = 0;
+  int maxi = string.length - 1;
+  /**/
+  for (i = maxi; 0 <= i; i--) {
+    cp = memchr(sample.at, string.at[i], sample.length);
+    if (cp != NULL) {
+      return maxi - i;
+    }
+  }
+  return string.length;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_span_not(Strview string, Strview sample)
+{
+  const char *cp = NULL;
+  int i = 0;
+  /**/
+  for (i = 0; i < string.length; i++) {
+    cp = memchr(sample.at, string.at[i], sample.length);
+    if (cp == NULL) {
+      return i;
+    }
+  }
+  return string.length;
+}
+
+/*----------------------------------------------------------*/
+int
+sv_span_not_end(Strview string, Strview sample)
+{
+  const char *cp = NULL;
+  int i = 0;
+  int maxi = string.length - 1;
+  /**/
+  for (i = maxi; 0 <= i; i--) {
+    cp = memchr(sample.at, string.at[i], sample.length);
+    if (cp == NULL) {
+      return maxi - i;
+    }
+  }
+  return string.length;
+}
+
+/*----------------------------------------------------------*/
+Strview
+sv_substr(Strview string, int start, int length)
+{
+  assert(start >= 0);
+  assert(start < string.length);
+  assert(length >= 0);
+  /**/
+  return sv_array(string.at + start, length);
+}
+
+/*----------------------------------------------------------*/
+int
+sv_suffix(Strview string, Strview suffix)
+{
+  const char *part = NULL;
+  /**/
+  if (string.length < suffix.length) {
+    return 0;
+  }
+  part = string.at + string.length - suffix.length;
+  return memcmp(part, suffix.at, suffix.length) == 0;
+}
+
+/*----------------------------------------------------------*/
 /* IMPLEMENTATION:                                          */
 /*----------------------------------------------------------*/
 
